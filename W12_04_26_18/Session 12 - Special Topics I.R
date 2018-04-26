41a31b7f06c8927c537eec6625354705be839a98
leslie-huang <lesliehuang@gmail.com>
2018-04-26 08:57:18 -0400

    cleaned up formatting

--- W12_04_26_18/Session 12 - Special Topics I.R
+++ W12_04_26_18/Session 12 - Special Topics I.R
@@ -7,7 +7,7 @@
 # Link: https://nyu.qualtrics.com/jfe/form/SV_3mGvDResIGQDtGZ
 
 
-setwd("/Users/lesliehuang/Text-as-Data-Lab-Spr2018/W13_04_26_18")
+setwd("/Users/lesliehuang/Text-as-Data-Lab-Spr2018/W12_04_26_18")
 
 #install.packages("bursts")
 library(bursts)
@@ -16,33 +16,40 @@ library(readtext)
 
 # 1  Loading bursty function: a repurposing of some guts of kleinberg()
 
-bursty <- function(word = "sioux",DTM, date) {
-  word.vec <- DTM[,which(colnames(DTM) == word)]
-  if(length(word.vec) == 0){
+bursty <- function(word = "sioux", DTM, date) {
+  word.vec <- DTM[, which(colnames(DTM) == word)]
+  if(length(word.vec) == 0) {
     print(word, " does not exist in this corpus.")
-  } else{
+  } 
+  else {
     word.times <- c(0,which(as.vector(word.vec)>0))
-    kl <- kleinberg(word.times, gamma=.5)
+    
+    kl <- kleinberg(word.times, gamma = 0.5)
     kl$start <- date[kl$start+1]
     kl$end <- date[kl$end]
     max_level <- max(kl$level)
+    
     plot(c(kl$start[1], kl$end[1]), c(1,max_level),
          type = "n", xlab = "Time", ylab = "Level", bty = "n",
          xlim = c(kl$start[1], kl$end[1]), ylim = c(1, max_level),
          yaxt = "n")
     axis(2, at = 1:max_level)
-    for(i in 1:nrow(kl)){
-      if(kl$start[i] != kl$end[i]){
+    
+    for (i in 1:nrow(kl)) {
+      if (kl$start[i] != kl$end[i]) {
         arrows(kl$start[i], kl$level[i], kl$end[i], kl$level[i], code = 3, angle = 90,
                length = 0.05)
-      } else{
+      } 
+      else {
         points(kl$start[i], kl$level[i])
       }
     }
+    
     print(kl)
   }
     #note deviation from standard defaults bec don't have that much data
 }
+
 
 # 2 Let's use this on the Conservative and Labour manifestos
 
